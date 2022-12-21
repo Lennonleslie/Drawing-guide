ff2b663daed979c3278a74d8d7c6938246e6a28d
lennonleslie <lennon.leslie@hotmail.com>
2022-12-21 18:24:52 +0800

    添加配置

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 生成静态文件， 
+# npm run docs:build
+# yarn docs:build
+pnpm vuepress build
+rm -rf ../Drawing-guide-page/dist/*
+
+# 将build生成的dist目录拷贝至上一层目录中
+cp -rf docs/.vuepress/dist ../Drawing-guide-page/
+
+# 进入生成的文件夹
+cd ../Drawing-guide-page/dist
+
+# git初始化，每次初始化不影响推送
+git init
+git add -A
+git commit -m 'deploy'
+git branch -M main
+
+# 如果你想要部署到 https://USERNAME.github.io
+# git push -f git@github.com:mengqiuleo/mengqiuleo.github.io.git main
+git push -f git@github.com:Lennonleslie/Drawing-guide.git main
