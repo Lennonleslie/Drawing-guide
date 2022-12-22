6e08aa089d123254dc05148d5052486fd4035951
lennonleslie <lennon.leslie@hotmail.com>
2022-12-22 15:52:03 +0800

    deploy-coding

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,7 +4,7 @@ export default defineUserConfig({
     lang: 'zh-CN',
     title: '你好， 设计师 ！',
     description: '这个是一个电子指南',
-    base: "/Drawingguide/",
+    "base": "/Drawingguide/",
     // base: './',
     // dest: './dest',
 })
