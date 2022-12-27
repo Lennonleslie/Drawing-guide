5e3564c144cfd8e235fc6e6380339c7ecc0d8252
lennonleslie <lennonleslie@hotmail.com>
2022-12-27 23:34:39 +0800

    deploy-coding

--- docs/.vuepress/configs/sidebar/index.js
+++ docs/.vuepress/configs/sidebar/index.js
@@ -9,40 +9,47 @@ export default {
       ],
     },
   ],
-    '/start/': [
-      {
-        text: "基础入门",
-        // 是否可折叠
-        // collapsible: true,
-        children: [
-          '数据原理',
-          '初步了解',
-          '结构解析',
-          '流程详解',
-        ],
-      },
-    ],
-    '/promote/': [
-      {
-        text: "进阶提升",
-        // 是否可折叠
-        collapsible: true,
-        children: [
-        ],
-      },
-    ],
-
-    '/tool/': [      
+  '/start/': [
     {
-      text: "工具",
-      collapsible: true,
+      text: "基础入门",
+      // 是否可折叠
+      // collapsible: true,
       children: [
-        {
-          text: "电脑工具",
-          children: ["常用电脑工具.md"],
-        },
-        "设计常用工具.md",
+        '数据原理',
+        '初步了解',
+        '结构解析',
+        '流程详解',
       ],
     },
-    ],
+  ],
+  '/promote/': [
+    {
+      text: "进阶提升",
+      // 是否可折叠
+      collapsible: true,
+      children: [
+      ],
+    },
+  ],
+
+  '/tool/': [
+    {
+      text: "工具",
+      // collapsible: true,
+      // children: [
+      //   {
+      //     text: "电脑工具",
+      //     children: [
+      //       "常用电脑工具",
+      //       "设计常用工具",
+      //     ],
+      //     text: "测量工具",
+      //     children: [
+      //       "常用电脑工具",
+      //       "设计常用工具",
+      //     ],
+      //   },
+      // ],
+    },
+  ],
 };
--- docs/tool/readme.md
+++ docs/tool/readme.md
@@ -1,9 +1,16 @@
-正常办公必备：
+### 正常办公必备
 
-浏览器：chrome
+浏览器：[chrome](https://www.google.cn/chrome/index.html)
 
-文件管理类
+远程软件：[todesk](https://www.todesk.com/download.html)
 
-搜索工具1：listary
+---
 
-搜索工具1：everything
+### 文件管理类
+
+搜索工具1：[listary](https://www.listary.com/)
+
+搜索工具1：[everything](https://www.voidtools.com/zh-cn/)
+
+---
+
