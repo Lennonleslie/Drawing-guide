70c3d13d2d56c0edb30aa70f008a8cf4ca29e794
lennonleslie <lennonleslie@hotmail.com>
2023-01-03 14:06:13 +0800

    deploy-coding

--- docs/.vuepress/.temp/internal/pagesRoutes.js
+++ docs/.vuepress/.temp/internal/pagesRoutes.js
@@ -5,11 +5,11 @@ export const pagesRoutes = [
   ["v-c95ddb30","/promote/",{"title":""},["/promote/index.html","/promote/readme.md"]],
   ["v-49df66ee","/promote/%E7%8E%8B%E7%9F%B3%E6%8B%86%E5%8D%95%E5%8A%9F%E8%83%BD.html",{"title":""},["/promote/王石拆单功能.html","/promote/%E7%8E%8B%E7%9F%B3%E6%8B%86%E5%8D%95%E5%8A%9F%E8%83%BD","/promote/王石拆单功能.md","/promote/%E7%8E%8B%E7%9F%B3%E6%8B%86%E5%8D%95%E5%8A%9F%E8%83%BD.md"]],
   ["v-1993c2d1","/theory/",{"title":""},["/theory/index.html","/theory/readme.md"]],
-  ["v-1559ca3e","/tool/",{"title":""},["/tool/index.html","/tool/readme.md"]],
   ["v-d72a4774","/start/",{"title":""},["/start/index.html","/start/readme.md"]],
   ["v-e45960aa","/start/%E5%88%9D%E6%AD%A5%E4%BA%86%E8%A7%A3.html",{"title":""},["/start/初步了解.html","/start/%E5%88%9D%E6%AD%A5%E4%BA%86%E8%A7%A3","/start/初步了解.md","/start/%E5%88%9D%E6%AD%A5%E4%BA%86%E8%A7%A3.md"]],
   ["v-a32684f8","/start/%E6%95%B0%E6%8D%AE%E5%8E%9F%E7%90%86.html",{"title":"数据原理"},["/start/数据原理.html","/start/%E6%95%B0%E6%8D%AE%E5%8E%9F%E7%90%86","/start/数据原理.md","/start/%E6%95%B0%E6%8D%AE%E5%8E%9F%E7%90%86.md"]],
   ["v-1df1bd88","/start/%E6%B5%81%E7%A8%8B%E7%AE%80%E8%A7%A3.html",{"title":""},["/start/流程简解.html","/start/%E6%B5%81%E7%A8%8B%E7%AE%80%E8%A7%A3","/start/流程简解.md","/start/%E6%B5%81%E7%A8%8B%E7%AE%80%E8%A7%A3.md"]],
   ["v-79f0d251","/start/%E7%BB%93%E6%9E%84%E8%A7%A3%E6%9E%90.html",{"title":"结构解析"},["/start/结构解析.html","/start/%E7%BB%93%E6%9E%84%E8%A7%A3%E6%9E%90","/start/结构解析.md","/start/%E7%BB%93%E6%9E%84%E8%A7%A3%E6%9E%90.md"]],
+  ["v-1559ca3e","/tool/",{"title":""},["/tool/index.html","/tool/readme.md"]],
   ["v-3706649a","/404.html",{"title":""},["/404"]],
 ]
