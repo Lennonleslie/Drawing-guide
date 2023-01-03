35468c23bf94302949c5a7ed9bc7beb40d7fd248
lennonleslie <lennonleslie@hotmail.com>
2023-01-03 16:04:59 +0800

    deploy-coding

--- docs/.vuepress/.temp/internal/pagesData.js
+++ docs/.vuepress/.temp/internal/pagesData.js
@@ -11,6 +11,8 @@ export const pagesData = {
   "v-15adcecc": () => import(/* webpackChunkName: "v-15adcecc" */"E:/GIT/Drawingguide/docs/.vuepress/.temp/pages/promote/DS王石拆单功能.html.js").then(({ data }) => data),
   // path: /promote/
   "v-c95ddb30": () => import(/* webpackChunkName: "v-c95ddb30" */"E:/GIT/Drawingguide/docs/.vuepress/.temp/pages/promote/index.html.js").then(({ data }) => data),
+  // path: /theory/
+  "v-1993c2d1": () => import(/* webpackChunkName: "v-1993c2d1" */"E:/GIT/Drawingguide/docs/.vuepress/.temp/pages/theory/index.html.js").then(({ data }) => data),
   // path: /start/
   "v-d72a4774": () => import(/* webpackChunkName: "v-d72a4774" */"E:/GIT/Drawingguide/docs/.vuepress/.temp/pages/start/index.html.js").then(({ data }) => data),
   // path: /start/%E5%88%9D%E6%AD%A5%E4%BA%86%E8%A7%A3.html
@@ -21,8 +23,6 @@ export const pagesData = {
   "v-1df1bd88": () => import(/* webpackChunkName: "v-1df1bd88" */"E:/GIT/Drawingguide/docs/.vuepress/.temp/pages/start/流程简解.html.js").then(({ data }) => data),
   // path: /start/%E7%BB%93%E6%9E%84%E8%A7%A3%E6%9E%90.html
   "v-79f0d251": () => import(/* webpackChunkName: "v-79f0d251" */"E:/GIT/Drawingguide/docs/.vuepress/.temp/pages/start/结构解析.html.js").then(({ data }) => data),
-  // path: /theory/
-  "v-1993c2d1": () => import(/* webpackChunkName: "v-1993c2d1" */"E:/GIT/Drawingguide/docs/.vuepress/.temp/pages/theory/index.html.js").then(({ data }) => data),
   // path: /tool/
   "v-1559ca3e": () => import(/* webpackChunkName: "v-1559ca3e" */"E:/GIT/Drawingguide/docs/.vuepress/.temp/pages/tool/index.html.js").then(({ data }) => data),
   // path: /404.html
